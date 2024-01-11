71c93beba92f97cf1ee3bbc32eef59ab5864f20a
Eduardo347387 <143133753+Eduardo347387@users.noreply.github.com>
2024-01-11 09:23:57 -0600

    implementado vista desktop

--- js/app.js
+++ js/app.js
@@ -5,14 +5,8 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     function toggleMenu(){
         const menu = document.querySelector('.menu');
-        
-        menu.style.display = (menu.style.display === 'flex') ? 
-        'none': 
-        'flex';
-
-        buttonHamburguesa.style.backgroundImage = (menu.style.display === 'flex') ?
-        'url("../images/icon-close.svg")' :
-        '';
+        menu.classList.toggle('ver')
+        buttonHamburguesa.classList.toggle('close')
     }
 
 })
